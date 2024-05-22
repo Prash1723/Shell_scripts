43cdde9521e84f6f77b7f8f186894bf0f8d0011c
Prash1723 <s.praveen795@gmail.com>
2024-05-22 23:17:31 +0530

    Comments

--- Video Resizer/video_sizer.sh
+++ Video Resizer/video_sizer.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-#read -p "Directory Path for the video :" location
-
-#cd "$location"
-
+# Video file location
 read -p "filename :" vid_file
 
+# Save file to specified location
 read -p "New filename :" new_file
 
+# Main Script
 ffmpeg -i "$vid_file" -vcodec libx265 -crf 28 "$new_file"
