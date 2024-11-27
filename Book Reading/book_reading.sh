ce66c106863d5d3735f6eb68008de82bd628c1f8
Prash1723 <s.praveen795@gmail.com>
2024-11-27 22:31:36 +0530

    visual\gaps

--- Book Reading/book_reading.sh
+++ Book Reading/book_reading.sh
@@ -2,9 +2,11 @@
 
 echo "Please answer the following questions -"
 echo "Which genre or topic do you want to read today?"
+echo " "
 ls ~/Books/
 read answer1
 echo "Which book do you want to read?"
+echo " "
 ls ~/Books/$answer1/
 read answer2
 xdg-open ~/Books/$answer1/"$answer2"
