0fe41027ce20059343a64e6c77df2b0ddd0030c5
praveen <s.praveen795@gmail.com>
2025-02-12 23:01:18 +0530

    update:book_reading.sh\ifelse

--- Book Reading/book_reading.sh
+++ Book Reading/book_reading.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-redshift -O 4500                     # For eye protection while reading, you can disable it by typing redshift -O 4500
+echo "Do you want to enable reading mode?"
+read answer1
+if [ $answer1 = "y" ]
+then 
+	redshift -O 4500                     # For eye protection while reading, you can disable it by typing redshift -O 4500
+else
+	echo "OK"
+fi
 echo "Please answer the following questions -"
 echo "Which genre or topic do you want to read today?"
 echo " "
 ls ~/Books/
-read answer1
+read answer2
 echo "Which book do you want to read?"
 echo " "
-ls ~/Books/$answer1/
-read answer2
-xdg-open ~/Books/$answer1/"$answer2"
+ls ~/Books/"$answer2"/
+read answer3
+xdg-open ~/Books/"$answer2"/"$answer3"
 
 clear
