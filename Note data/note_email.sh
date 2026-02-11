c91adc19a5ab87e65b40c49fb9ba66ab26e95161
praveen <s.praveen795@gmail.com>
2026-02-11 22:38:18 +0530

    update\note

--- Note data/note_email.sh
+++ Note data/note_email.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "Enter name of the company:"; read -p name
-echo "Enter email address:"; read -p email
-echo "Enter location:"; read -p location
+note_data (
+	echo "Enter name of the company:"; read -p name
+	echo "Enter email address:"; read -p email
+	echo "Enter location:"; read -p location
 
-$name,$email,$location >> ~/Documents/Jobs/Emailaddress.csv
+	$name,$email,$location >> ~/Documents/Jobs/Emailaddress.csv
+)
 
 clear
+
+note_data()
